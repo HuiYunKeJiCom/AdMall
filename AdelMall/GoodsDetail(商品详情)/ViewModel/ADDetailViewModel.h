f719f5b2495e71a48d51875847b417daf542e481
yangxinda <842440642@qq.com>
2018-04-12 10:12:15 +0800

    测试sourceTree和git update by CTO

--- AdelMall/GoodsDetail(商品详情)/ViewModel/ADDetailViewModel.h
+++ AdelMall/GoodsDetail(商品详情)/ViewModel/ADDetailViewModel.h
@@ -12,4 +12,16 @@
 
 @property (nonatomic,readonly)UICollectionView *detailView;//相当于一个工厂方法，可以直接提取使用，加载到外部的view上，内部实现为懒加载。注意，初始的frame为(0,0,0,0)，需要重新设置frame
 
+
+/*
+    add by CTO
+    测试git
+    123321
+    1234567
+ 
+ */
+
+
+
+
 @end
