c9f39c176962c47d22fe569037e394b976a6111c
yangxinda <undeadableheart@gmail.com>
2018-04-17 13:55:01 +0800

    运行通过备份

---
+++ AdelMall/GoodsDetail(商品详情)/Ctrl/ADGoodsDetailViewController_LOCAL_26661.h
@@ -0,0 +1,15 @@
+//
+//  ADGoodsDetailViewController.h
+//  AdelMall
+//
+//  Created by apple on 2018/4/11.
+//  Copyright © 2018年 Adel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADGoodsDetailViewController : UIViewController
+
+@property (nonatomic,copy) NSString *goodsID;//商品id
+
+@end
