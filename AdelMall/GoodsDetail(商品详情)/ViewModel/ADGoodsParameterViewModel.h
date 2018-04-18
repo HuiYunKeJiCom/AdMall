8646e413f9fb5cee268ded34873be53426fd89b0
yangxinda <undeadableheart@gmail.com>
2018-04-18 15:58:26 +0800

    update by CTO

---
+++ AdelMall/GoodsDetail(商品详情)/ViewModel/ADGoodsParameterViewModel.h
@@ -0,0 +1,13 @@
+//
+//  ADGoodsParameterViewModel.h
+//  AdelMall
+//
+//  Created by apple on 2018/4/18.
+//  Copyright © 2018年 Adel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADGoodsParameterViewModel : NSObject
+
+@end
---
+++ AdelMall/GoodsDetail(商品详情)/ViewModel/ADGoodsParameterViewModel.m
@@ -0,0 +1,13 @@
+//
+//  ADGoodsParameterViewModel.m
+//  AdelMall
+//
+//  Created by apple on 2018/4/18.
+//  Copyright © 2018年 Adel. All rights reserved.
+//
+
+#import "ADGoodsParameterViewModel.h"
+
+@implementation ADGoodsParameterViewModel
+
+@end
